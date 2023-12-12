d877f62ca1040308c4dc311f6f1b05b0d121d904
Gymhgy <49801033+Gymhgy@users.noreply.github.com>
2023-12-11 22:35:37 -0800

    12

--- utils.py
+++ utils.py
@@ -8,3 +8,9 @@ def ans(obj):
     cmd='echo '+str(obj).strip()+'|clip'
     subprocess.check_call(cmd, shell=True)
     return obj
+
+def neighbors4(x, y):
+    return (x+1, y), (x, y+1), (x-1, y), (x, y-1)
+
+def neighbors8(x, y):
+    return (x+1, y), (x, y+1), (x-1, y), (x, y-1), (x+1, y+1), (x-1, y+1), (x-1, y-1), (x+1, y-1)
